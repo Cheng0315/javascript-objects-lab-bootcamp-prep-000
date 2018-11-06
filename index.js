2552318ecf7aa83d77e8f4fe34b691e18ec6e284
Cheng Vue <cvu3@ucsc.edu>
2018-11-06 02:16:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   delete object[key];
-  return object
+  return object;
 }
 
 function deleteFromObjectByKey(object, key) {
-  Object.assign({}, object, delete object[key]);
+  return Object.assign({}, object, delete object[key]);
 }
